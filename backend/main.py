72bb7d4181cf57295b4deb897152b6592a85c336
Ushindi Sidi <sidiushindi@gmail.comsource>
2026-04-23 12:51:03 +0300

    ..

--- backend/main.py
+++ backend/main.py
@@ -21,7 +21,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173", 
-        "https://scam-radar-xi.vercel.app/" 
+        "https://scam-radar-xi.vercel.app" 
     ],
     allow_credentials=True,
     allow_methods=["*"],
